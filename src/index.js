41d57476e0cb14c5bc6eee35d018651c686c7df4
Nino Filiu <nino.filiu@gmail.com>
2020-12-26 00:50:13 +0100

    eslint + airbnb config setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp } from 'vue';
+import App from './App.vue';
 
 const app = createApp(App);
-app.mount("#app");
+app.mount('#app');
 
 if (import.meta.hot) {
   import.meta.hot.accept();
