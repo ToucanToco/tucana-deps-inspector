94b75c803ece3b84a2f531858672534e4e31e36b
Nino Filiu <nino.filiu@gmail.com>
2021-06-18 10:36:11 +0200

    optimize and display ETA

--- build.sh
+++ build.sh
@@ -8,52 +8,52 @@ then
 fi
 tucana_path=$1
 
-# Computes dependencies
-compute_dependencies () {
-  for file in $files
-  do
-    requires=$(cat $file | grep -o "require('[^']*')" | colrm 1 9 | rev | colrm 1 2 | rev)
-    imports=$(cat $file | grep '^import ' | grep -o "'[^']*';$" | colrm 1 1 | rev | colrm 1 2 | rev)
-    multiline_imports=$(cat $file | grep '^} from ' | colrm 1 8 | rev | colrm 1 2 | rev)
-    exports=$(cat $file | grep '^export .* from ' | grep -o "'.*'" | colrm 1 1 | rev | colrm 1 1 | rev)
-    async_oneline_imports=$(cat $file | grep "[^{]import(.*'\S*')" | grep -o "'[^']*'" | colrm 1 1 | rev | colrm 1 1 | rev)
-    async_multiline_imports=$(cat $file | grep -A 1 "import($" | grep -o "'\S*'" | colrm 1 1 | rev | colrm 1 1 | rev)
-    deps=$(echo $imports $multiline_imports $exports $requires $async_oneline_imports $async_multiline_imports | fmt -w 1 | grep -v '^[a-z]' | grep -v '@[a-z]' | grep -E -v '(json|png|pug|scss|svg)$')
-    for dep in $deps
-    do
-      # resolves the @/ prefix, cf tsconfig and jsconfig
-      dep=$(echo $dep | sed 's/^@/src/')
-
-      # resolve the relative dependencies
-      if [[ $dep ==  .* ]]
-      then
-        dep=$(realpath --relative-to=$PWD $(dirname $file)/$dep)
-      fi
-
-      # resolves the extension or index
-      for ext in .js .ts .vue /index.js /index.ts
-      do
-        if [ -f $dep$ext ]; then dep=$dep$ext; break; fi
-      done
-      
-      echo $file,$dep >> $out
-    done
-  done
-}
-
-out=$PWD/deps.csv
-echo > $out
-cd $tucana_path
-files=$(echo src/**/*.{vue,js,ts} | fmt -w 1 | grep -E -v '(test|specs|storybook).(js|ts)' | grep -v __mocks__ | grep -v fixtures)
-compute_dependencies
-cd $OLDPWD
-
+# compute dependencies
 out=$PWD/extended-deps.csv
 echo > $out
 cd $tucana_path
 files=$(echo src/**/*.{vue,js,ts})
-compute_dependencies
+nb_files=$(echo $files | fmt -w 1 | wc -l)
+i=0
+t0=$(date +%s)
+echo "processing $nb_files files"
+echo
+for file in $files
+do
+  requires=$(cat $file | grep -o "require('[^']*')" | colrm 1 9 | rev | colrm 1 2 | rev)
+  imports=$(cat $file | grep '^import ' | grep -o "'[^']*';$" | colrm 1 1 | rev | colrm 1 2 | rev)
+  multiline_imports=$(cat $file | grep '^} from ' | colrm 1 8 | rev | colrm 1 2 | rev)
+  exports=$(cat $file | grep '^export .* from ' | grep -o "'.*'" | colrm 1 1 | rev | colrm 1 1 | rev)
+  async_oneline_imports=$(cat $file | grep "[^{]import(.*'\S*')" | grep -o "'[^']*'" | colrm 1 1 | rev | colrm 1 1 | rev)
+  async_multiline_imports=$(cat $file | grep -A 1 "import($" | grep -o "'\S*'" | colrm 1 1 | rev | colrm 1 1 | rev)
+  deps=$(echo $imports $multiline_imports $exports $requires $async_oneline_imports $async_multiline_imports | fmt -w 1 | grep -v '^[a-z]' | grep -v '@[a-z]' | grep -E -v '(json|png|pug|scss|svg)$')
+  for dep in $deps
+  do
+    # resolves the @/ prefix, cf tsconfig and jsconfig
+    dep=$(echo $dep | sed 's/^@/src/')
+
+    # resolve the relative dependencies
+    if [[ $dep ==  .* ]]
+    then
+      dep=$(realpath --relative-to=$PWD $(dirname $file)/$dep)
+    fi
+
+    # resolves the extension or index
+    for ext in .js .ts .vue /index.js /index.ts
+    do
+      if [ -f $dep$ext ]; then dep=$dep$ext; break; fi
+    done
+    
+    echo $file,$dep >> $out
+  done
+
+  i=$(($i + 1))
+  t1=$(date +%s)
+  echo -e "\033[1Aprogress: $((100 * $i / $nb_files))%, ETA: $((($t1 - $t0) * ($nb_files - $i) / $i))s     "
+done
 cd $OLDPWD
+
+cat extended-deps.csv | grep -E -v '(test|specs|storybook).(js|ts)' | grep -v __mocks__ | grep -v fixtures > deps.csv
 
 # Builds ./unimported.txt
 imported=$(cat extended-deps.csv | cut -d ',' -f 2 | sort | uniq)
